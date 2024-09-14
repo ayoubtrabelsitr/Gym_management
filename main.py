7aeb5b6b1fa41edff9fdbb29d551775e62dfff41
ayoubtrabelsitr <ayoubtrabelsitr@gmail.com>
2024-09-14 18:14:47 +0200

    home_pageV1

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ import mysql.connector
 
 
 window=Tk()
+frame_name = Frame(window)
+frame_name.place(relx=0.5, rely=0.6, anchor="center")
+frame_password=Frame(window)
+frame_password.place(relx=0.5, rely=0.7, anchor="center")
 backgrounf_color_home='red'
 window.title("BrothersGym")
 window.geometry("700x450")
+window.minsize(500,350)
 window.config(bg=backgrounf_color_home)
+#image
+width=height=180
+image_home = PhotoImage(file="jeu.png").subsample(3)
+canvas = Canvas(window,width=width, height=height,bg=backgrounf_color_home, highlightthickness=0)
+canvas.create_image(width/2, height/2, image=image_home)
+canvas.place(relx=0.5, rely=0.3, anchor="center")
 
-name = Label(window,text = "Username",background=backgrounf_color_home).place(x = 180,y = 200)  
-e1 = Entry(window).place(x = 240,y = 200) 
-password = Label(window,text = "Password",background=backgrounf_color_home).place(x = 180,y = 230)   
-e2 = Entry(window).place(x = 240,y = 230) 
-submit = Button(window, text = "Submit").place(x = 280,y = 280)   
+name = Label(frame_name,text = "Username",background=backgrounf_color_home).pack(side=LEFT) 
+e1 = Entry(frame_name).pack(side=RIGHT) 
+password = Label(frame_password,text = "Password",background=backgrounf_color_home).pack(side=LEFT)   
+e2 = Entry(frame_password).pack(side=RIGHT)
+submit = Button(window, text = "Connect").place(relx=0.52, rely=0.8, anchor="center")
 window.mainloop()
 
 
